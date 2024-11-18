484e796fdacec2660406f2074871b2d294ebbd69
AFBGdev <anresfb@gmail.com>
2024-11-18 15:10:25 -0500

    feat: :sparkles: add availability to fetch links from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
+import { useEffect, useState } from 'react';
 import { CreateLinkForm } from './components/create-link-form';
 import { LinksList } from './components/links-list';
-import { LinkType } from './types/types';
-
-const DATA:LinkType[] = [
-  {
-    id: '54468f88-9da5-40f1-805e-47f7418cb74d',
-    targetUrl: 'https://localhost:8000',
-    slug: 'shorturl',
-    redirectUrl: 'https://localhost:8000'
-  },
-  {
-    id: 'b9273c11-8855-4b68-9ad8-b7ec84d61fd0',
-    targetUrl: 'https://localhost:8000',
-    slug: 'shorturl',
-    redirectUrl: 'https://localhost:8000'
-  }
-]
+import { type LinkType } from './types/types';
+import { getLinks } from './api/links.requests';
 
 function App() {
+  const [links, setLinks] = useState<LinkType[]>([]);
+
+  useEffect(() => {
+    getLinks()
+      .then(data => {
+        setLinks(data)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }, [])
+
   return (
     <main className="max-w-3xl mx-auto flex flex-col items-center">
       <h1 className="text-3xl font-bold mb-6 text-center">
@@ -29,7 +28,7 @@ function App() {
       <section className="w-full sm:max-w-md p-3 sm:rounded-xl sm:bg-white/50 sm:backgrop-blur flex flex-col gap-4">
         <CreateLinkForm />
 
-        <LinksList data={DATA} />
+        <LinksList data={links} />
       </section>
     </main>
   )
