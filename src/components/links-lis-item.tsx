449ced27d35a643db7630323568caa463fc98f61
AFBGdev <anresfb@gmail.com>
2024-11-18 10:00:22 -0500

    fix: :lipstick: fix links list item style

--- src/components/links-lis-item.tsx
+++ src/components/links-lis-item.tsx
@@ -9,21 +9,31 @@ interface LinksLisItemProps {
 export function LinksLisItem({ link }: LinksLisItemProps) {
   return (
     <li
-    className='w-full flex flex-col'
+    className='w-full flex flex-col rounded-xl border border-gray-200 bg-white py-3 px-4 shadow-lg gap-1.5'
     >
-      <div className='w-full flex gap-2'>
-        <a href={link.redirectUrl} target="_blank" rel="noopener noreferrer">
+      <div className='w-full flex gap-3 justify-start items-center'>
+        <a
+          className='font-semibold text-gray-800 hover:text-black'
+          href={link.redirectUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           {link.slug}
         </a>
 
-        <IoCopyOutline />
+        <button
+          className='rounded-full transition-all duration-75 border border-gray-200 bg-gray-50 hover:scale-100 hover:bg-gray-100 active:bg-gray-100 p-1.5'
+          type='button'
+        >
+          <IoCopyOutline size={18} />
+        </button>
       </div>
 
       <div className='w-full flex gap-2'>
-        <BsArrowReturnRight size={25} />
+        <BsArrowReturnRight className='text-gray-400' size={18} />
 
         <a
-          className='max-w-60 sm:max-w-72 truncate text-sm text-gray-400 underline-offset-4 transition-all hover:text-gray-700 hover:underline'
+          className='max-w-60 sm:max-w-72 truncate text-sm text-gray-400 underline-offset-4 transition-all hover:text-gray-700 hover:underline cursor-pointer'
         >
           {link.targetUrl}
         </a>
